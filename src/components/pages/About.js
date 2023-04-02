0fe95bb7936abba2cac7f956346d7025c3e1e61a
AlastairHatfield <alastairhatfield@live.com>
2023-04-02 05:40:05 +0100

    helpers added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,59 @@ import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        sample text sample text sample text sample text sample text sample text
-      </p>
-    </div>
+    <section class="projects-section" id="projects">
+      <h1 class="section-title">
+        Recent <span>Projects</span>
+      </h1>
+      <div class="project-horiseon">
+        <h1>Project 1</h1>
+        <h2>Animl.</h2>
+        <p>
+          This project was undertaken as a group collaboration, designed for
+          animal lovers!
+        </p>
+        <a
+          class="project-img1"
+          href="https://alastairhafield.github.io/Animl-Project/home.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="img/homepage.png" alt="img" />
+        </a>
+      </div>
+      <div class="projects-container2">
+        <div class="project-studyguide">
+          <h1>Project 2:</h1>
+
+          <h2>Studdy Buddy!</h2>
+          <p>
+            Studdy Buddy is a student hub for students to share coding problmes
+            and solutions while also being able to connect to tutors and other
+            students.
+          </p>
+          <a
+            class="project-img2"
+            href="https://studdybuddy.herokuapp.com/login"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="img/questions.jpg" alt="img" />
+          </a>
+        </div>
+        <div class="project-studentbio">
+          <h1>Project 3:</h1>
+          <h2>Password Generator</h2>
+          <p>Generate a secure password!</p>
+          <a
+            class="project-img3"
+            href="https://alastairhafield.github.io/password-gen-alastair/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="img/passwordGen (1).jpg" alt="img" />
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Contact sample text Contact sample text Contact sample text Contact
-        sample
-      </p>
-    </div>
+    <section>
+      <div>
+        <h1>Contact Page</h1>
+        <p>
+          Contact sample text Contact sample text Contact sample text Contact
+          sample
+        </p>
+      </div>
+    </section>
   );
 }
