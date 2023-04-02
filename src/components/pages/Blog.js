2b2d4044d75e66432dfd1cfd9692a89e2594be1e
AlastairHatfield <alastairhatfield@live.com>
2023-04-02 10:30:39 +0100

    contact form added

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,12 +1,94 @@
-import React from "react";
+import { useState } from "react";
+import { validateEmail } from "./utils/helpers.js";
 
-export default function Blog() {
+export default function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !email) {
+      setErrorMessage("Fill out name and email address");
+      return;
+    }
+
+    console.log(formState);
+  }
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (e.target.name === "name") {
+      setFormState({ ...formState, name: e.target.value });
+    }
+
+    if (e.target.name === "email") {
+      setFormState({ ...formState, email: e.target.value });
+    }
+
+    if (e.target.name === "message") {
+      setFormState({ ...formState, message: e.target.value });
+    }
+  }
+
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Blog sample text Blog sample text Blog sample text Blog sample text Blog
-      </p>
-    </div>
+    <section className="container">
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            onBlur={handleChange}
+            name="name"
+          />
+        </div>
+        <div>
+          <label>Email address:</label>
+          <input
+            type="email"
+            defaultValue={email}
+            name="email"
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label>Message:</label>
+          <textarea
+            name="message"
+            defaultValue={message}
+            onBlur={handleChange}
+            rows="5"
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 }
--- src/components/pages/utils/helpers.js
+++ src/components/pages/utils/helpers.js
@@ -1,5 +1,5 @@
 //export check email function
-export function checkEmail(email) {
+export function validateEmail(email) {
   return email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
 }
 
